e371a3bbb06713f9085cd9b01b5493c73f3b2594
Asher Shaheen <ashershaheen@gmail.com>
2017-09-01 14:16:09 -0400

    seeded files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901174042) do
+ActiveRecord::Schema.define(version: 20170901180518) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170901174042) do
     t.integer "teacher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
     t.index ["teacher_id"], name: "index_courses_on_teacher_id"
   end
 
