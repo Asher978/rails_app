663b0cf141303e950119b1a968db6762e1dad023
Asher Shaheen <ashershaheen@gmail.com>
2017-09-01 15:30:44 -0400

    added views and controllers for rendering

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,3 +1,9 @@
 class TeachersController < ApplicationController
-    
+    def index 
+        @teacher = Teacher.all
+    end
+
+    def show
+        @teacher = Teacher.find(params[:id])
+    end
 end
